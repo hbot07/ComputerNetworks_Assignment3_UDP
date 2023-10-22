798e308d4c64c59b8365370f79061beaf35b6742
Parth <hbot07@gmail.com>
2023-10-22 06:09:55 +0530

    aimd and burst implemented

--- milestone 2/constant burst size.py
+++ milestone 2/constant burst size.py
@@ -1,5 +1,7 @@
 import socket
 import time
+from colorama import Fore
+import pickle
 
 start_time = time.time()
 
@@ -9,7 +11,12 @@ SERVER_PORT = 9801
 BUFFER_SIZE = 2048
 TIMEOUT = 0.025  # Timeout in seconds
 MAX_RETRIES = 5
+batch_size = 10
 
+graph_send = []
+graph_rcv = []
+graph_burst_send = []
+graph_burst_rcv = []
 
 def get_data_from_server(offset, num_bytes):
     retries = 0
@@ -36,6 +43,52 @@ def get_data_from_server(offset, num_bytes):
     print(f"Failed to fetch data for Offset: {offset} after {MAX_RETRIES} retries.")
     return None
 
+def send_request(offset, num_bytes):
+    graph_send.append((offset, (time.time()-start_time)*1000))
+    message = f"Offset: {offset}\nNumBytes: {num_bytes}\n\n"
+    client_socket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+    graph_burst_send.append((batch_size, (time.time() - start_time) * 1000))
+    return 1
+
+def receive_data(received_data):
+    try:
+        data, addr = client_socket.recvfrom(BUFFER_SIZE)
+        data_splitted = data.decode().split('\n\n')
+        offset = int(data_splitted[0].split('\n')[0].split(':')[1])
+        received_data[offset//CHUNK_SIZE] = data_splitted[1]
+        graph_rcv.append((offset, (time.time() - start_time)*1000))
+        graph_burst_rcv.append((1 if "Squished" in data_splitted[0] else 0, (time.time() - start_time) * 1000))
+        return 1
+    except:
+        # print(Fore.RED + "Didn't get anything" + Fore.RESET)
+        return 0
+
+def get_data_one_pass():
+    global batch_size
+
+    sent, rcvd = 0, 0
+    # send reqs
+    batch_count = 0
+    for i in range(len(received_data)):
+        if received_data[i] is None:
+            offset = i * CHUNK_SIZE
+            sent += send_request(offset, CHUNK_SIZE)
+            batch_count += 1
+
+        if batch_count == batch_size:
+            break
+
+    while True:
+        result = receive_data(received_data)
+        if result == 0:
+            break
+        rcvd += result
+
+    # if sent == rcvd:
+    #     batch_size += 1
+    # else:
+    #     batch_size = max(batch_size//2, 1)
+
 
 if __name__ == "__main__":
     # Create a UDP socket
@@ -58,18 +111,17 @@ if __name__ == "__main__":
     CHUNK_SIZE = 1448
     received_data = [None for i in range(0, total_size, CHUNK_SIZE)]
 
-    def get_data_one_pass():
-        for offset in range(0, total_size, CHUNK_SIZE):
-            data_chunk = get_data_from_server(offset, min(CHUNK_SIZE, total_size-offset))
-            if data_chunk:
-                received_data[offset//CHUNK_SIZE] = data_chunk
-                offset += min(CHUNK_SIZE, total_size-offset)
+    passes = 0
+    while received_data.count(None) > 0:
+        passes += 1
+        print(Fore.YELLOW + f"Pass {passes}. remaining: {received_data.count(None)}" + Fore.RESET)
+        get_data_one_pass()
 
-    get_data_one_pass()
-
-    received_data = "".join(received_data)
+    received_data = "".join(received_data)[:-1]
     print(f"Received {len(received_data)}/{total_size} bytes of data.")
 
+    with open("received_data.txt", 'w') as f:
+        f.write(received_data)
 
     # Submit
     import hashlib
@@ -93,3 +145,13 @@ if __name__ == "__main__":
             client_socket.sendto(str.encode("Submit: testing\n" + str(md5) + "\n\n"), (SERVER_IP, SERVER_PORT))
 
     print(f"time taken: {time.time() - start_time}s")
+    print(f"number of passes: {passes}")
+
+    with open("graph_rcv.pkl", 'wb') as f:
+        pickle.dump(graph_rcv, f)
+    with open("graph_send.pkl", 'wb') as f:
+        pickle.dump(graph_send, f)
+    with open("graph_burst_send.pkl", 'wb') as f:
+        pickle.dump(graph_burst_send, f)
+    with open("graph_burst_rcv.pkl", 'wb') as f:
+        pickle.dump(graph_burst_rcv, f)
--- milestone 2/gen graphs.py
+++ milestone 2/gen graphs.py
@@ -1,3 +1,4 @@
+NAME = "constant burst size 10"
 import pickle
 
 import matplotlib.pyplot as plt
@@ -17,9 +18,9 @@ plt.scatter(graph_time_send, graph_offset_send, color='blue')
 plt.scatter(graph_time_recv, graph_offset_recv, color='orange')
 plt.xlabel("Time")
 plt.ylabel("Offset")
-plt.title("Sequence-number trace")
+plt.title(f"Sequence-number trace {NAME}")
 plt.grid()
-plt.savefig("Sequence-number trace.jpeg")
+plt.savefig(f"graphs/{NAME}_1.jpeg")
 plt.show()
 
 zoom = len([i for i in graph_time_recv if i < 500])
@@ -28,7 +29,29 @@ plt.scatter(graph_time_send[:zoom], graph_offset_send[:zoom], color='blue')
 plt.scatter(graph_time_recv[:zoom], graph_offset_recv[:zoom], color='orange')
 plt.xlabel("Time")
 plt.ylabel("Offset")
-plt.title("Zoomed in sequence-number trace")
+plt.title(f"Zoomed in sequence-number trace {NAME}")
 plt.grid()
-plt.savefig("Zoomed in sequence-number trace.jpeg")
+plt.savefig(f"graphs/{NAME}_2.jpeg")
+plt.show()
+
+with open("graph_burst_send.pkl", 'rb') as f:
+    graph_burst_send = pickle.load(f)
+with open("graph_burst_rcv.pkl", 'rb') as f:
+    graph_burst_rcv = pickle.load(f)
+
+plt.figure(figsize=(10, 5), dpi=150)
+plt.plot([i[1] for i in graph_burst_send], [i[0] for i in graph_burst_send], "-o", color='blue')
+plt.xlabel("Time")
+plt.ylabel("Burst Size")
+
+# Create a twin of the original y-axis
+ax2 = plt.gca().twinx()
+# Plot the data on the twin y-axis
+ax2.plot([i[1] for i in graph_burst_rcv], [i[0] for i in graph_burst_rcv], "-o",color='orange')
+ax2.set_ylabel('Squished', color='orange')
+ax2.tick_params('y', colors='orange')
+ax2.set_ylim([-0.1, 1.75])
+
+plt.grid()
+plt.savefig(f"graphs/{NAME}_3.jpeg")
 plt.show()
